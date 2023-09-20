a5479ee84eaf78bcd65abfc062624e51220e60c7
arsuhinars <arsuhinars@gmail.com>
2023-09-20 17:28:59 +0400

    Added running app module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::process::ExitCode;
+
 pub mod utils;
 pub mod raycaster;
-pub mod renderer;
+pub mod rendering;
+pub mod app;
 
 #[cfg(test)]
 pub mod tests;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> ExitCode {
+    match app::App::init() {
+        Ok(mut app) => {
+            app.run();
+            ExitCode::SUCCESS
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+            ExitCode::FAILURE
+        }
+    }
 }
