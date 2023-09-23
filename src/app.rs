d237e13be1481ae9602a05b092d4a4e7047c50d6
arsuhinars <arsuhinars@gmail.com>
2023-09-23 23:17:52 +0400

    app module updates

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
-use imgui_glow_renderer::AutoRenderer;
+use std::rc::Rc;
+
 use sdl2;
-use glow::{self, HasContext, COLOR_BUFFER_BIT};
+use imgui_glow_renderer::{AutoRenderer as ImguiRenderer, TextureMap as ImguiTextureMap};
+use glow::{self, HasContext};
 use imgui;
 
 use crate::rendering::camera::Camera;
 use crate::rendering::lightning::{DiffuseDirectLightning, UnlitLightning};
 use crate::rendering::material::{CheckerMaterial, SolidMaterial};
+use crate::rendering::pixel_canvas::PixelCanvas;
 use crate::rendering::renderer::Renderer;
 use crate::raycaster::{SphereRaycaster, BoxRaycaster, Raycaster};
 use crate::utils::{color::Color, vector::Vector3};
@@ -13,11 +16,11 @@ use crate::utils::{color::Color, vector::Vector3};
 const APP_WINDOW_TITLE: &str = "retro_cube";
 const APP_WINDOW_WIDTH: u32 = 640;
 const APP_WINDOW_HEIGHT: u32 = 480;
-const APP_RENDER_WIDTH: u32 = 160;
-const APP_RENDER_HEIGHT: u32 = 120;
+const APP_RENDER_WIDTH: usize = 160;
+const APP_RENDER_HEIGHT: usize = 120;
 
 fn build_renderer(
-    texture_creator: sdl2::render::TextureCreator<sdl2::video::WindowContext>
+    gl: Rc<glow::Context>, texture_map: &mut dyn ImguiTextureMap
 ) -> Result<Renderer, String> {
     let mut camera = Camera::default();
     camera.get_mut_transform().set_position(&Vector3::new(0.0, 0.0, -2.0));
@@ -43,18 +46,22 @@ fn build_renderer(
         )
     );
 
-    let render_texture = texture_creator.create_texture_streaming(
-        sdl2::pixels::PixelFormatEnum::RGBA8888, APP_RENDER_WIDTH, APP_RENDER_HEIGHT
-    ).map_err(|err| err.to_string())?;
+    let pixel_canvas = PixelCanvas::new(
+        gl, texture_map, APP_RENDER_WIDTH, APP_RENDER_HEIGHT
+    )?;
+
+    // let render_texture = texture_creator.create_texture_streaming(
+    //     sdl2::pixels::PixelFormatEnum::RGBA8888, APP_RENDER_WIDTH, APP_RENDER_HEIGHT
+    // ).map_err(|err| err.to_string())?;
 
     Ok(
         Renderer::new(
-            camera, raycaster, material, lightning, render_texture
+            camera, raycaster, material, lightning, pixel_canvas
         )
     )
 }
 
-fn build_imgui(gl: glow::Context, window: &sdl2::video::Window) -> Result<(imgui::Context, AutoRenderer), String> {
+fn build_imgui(gl: glow::Context, window: &sdl2::video::Window) -> Result<(imgui::Context, ImguiRenderer), String> {
     let mut imgui = imgui::Context::create();
     imgui.set_ini_filename(None);
     imgui.set_log_filename(None);
@@ -69,7 +76,7 @@ fn build_imgui(gl: glow::Context, window: &sdl2::video::Window) -> Result<(imgui
         (window_drawable_size.1 as f32) / (window_size.1 as f32),
     ];
 
-    let renderer = AutoRenderer::initialize(
+    let renderer = ImguiRenderer::initialize(
         gl, &mut imgui
     ).map_err(|err| err.to_string())?;
 
@@ -87,13 +94,13 @@ pub struct App {
     video: sdl2::VideoSubsystem,
     timer: sdl2::TimerSubsystem,
     event_pump: sdl2::EventPump,
-    // canvas: sdl2::render::WindowCanvas,
+    canvas: sdl2::render::WindowCanvas,
     gl_context: sdl2::video::GLContext,
-    window: sdl2::video::Window,
+    // window: sdl2::video::Window,
     imgui: imgui::Context,
-    imgui_renderer: AutoRenderer,
+    imgui_renderer: ImguiRenderer,
 
-    // renderer: Renderer,
+    renderer: Renderer,
     is_running: bool,
     last_ticks: u32,
     delta_time: f32
@@ -118,12 +125,16 @@ impl App {
 
         let timer = sdl_context.timer()?;
 
-        // let canvas = window.into_canvas().build().map_err(|err| err.to_string())?;
+        let canvas = window.into_canvas().build().map_err(|err| err.to_string())?;
 
-        // let renderer = build_renderer(canvas.texture_creator())?;
+        let gl = glow_context(canvas.window());
 
-        let gl = glow_context(&window);
-        let (imgui, imgui_renderer) = build_imgui(gl, &window)?;
+        let (imgui, mut imgui_renderer) = build_imgui(gl, canvas.window())?;
+
+        let renderer = build_renderer(
+            imgui_renderer.gl_context().clone(),
+            imgui_renderer.texture_map_mut()
+        )?;
 
         let event_pump = sdl_context.event_pump()?;
 
@@ -132,13 +143,13 @@ impl App {
             video,
             timer,
             event_pump,
-            window,
+            // window,
             gl_context,
-            // canvas,
+            canvas,
             imgui,
             imgui_renderer,
 
-            // renderer,
+            renderer,
             is_running: true,
             last_ticks: 0,
             delta_time: 0.0
@@ -161,20 +172,28 @@ impl App {
         // self.canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
         // self.canvas.clear();
 
-        // self.renderer.render();
+        self.renderer.render();
+
         // self.canvas.copy(
         //     &self.renderer.get_mut_render_texture(), None, None
         // ).expect("unable to copy render texture on window");
-        // unsafe { self.canvas.render_flush(); }
+        // unsafe { self.canvas.render_flush() };
+        // self.canvas.present();
 
         let ui = self.imgui.new_frame();
         ui.show_demo_window(&mut true);
 
+        unsafe { self.imgui_renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+
+        self.renderer.get_pixel_canvas().render(
+            ui,
+            [APP_WINDOW_WIDTH as f32, APP_WINDOW_HEIGHT as f32]
+        );
+
         let ui_draw_data = self.imgui.render();
-        unsafe { self.imgui_renderer.gl_context().clear(COLOR_BUFFER_BIT) };
         self.imgui_renderer.render(ui_draw_data).expect("unable to render ui");
         
-        self.window.gl_swap_window();
+        self.canvas.window().gl_swap_window();
 
         // self.canvas.present();
     }
