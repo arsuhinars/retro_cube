3c71850ebf72e8041975452b67e661fc249fb389
arsuhinars <arsuhinars@gmail.com>
2023-09-24 17:37:53 +0400

    moved renderer builder to module & cleaned code

--- src/rendering/renderer.rs
+++ src/rendering/renderer.rs
@@ -1,12 +1,13 @@
-// use sdl2::render::Texture;
-
 use std::cell::{RefCell, Ref};
+use std::rc::Rc;
 
-use crate::raycaster::{Raycaster, RaycastHit};
+use imgui_glow_renderer::TextureMap as ImguiTextureMap;
+
+use crate::raycaster::{Raycaster, RaycastHit, BoxRaycaster};
 use crate::utils::{color::Color, vector::Vector3};
 use super::camera::Camera;
-use super::lightning::Lightning;
-use super::material::Material;
+use super::lightning::{Lightning, DiffuseDirectLightning};
+use super::material::{Material, CheckerMaterial};
 use super::pixel_canvas::PixelCanvas;
 
 pub struct Renderer {
@@ -14,51 +15,16 @@ pub struct Renderer {
     camera: Camera,
     raycaster: Box<dyn Raycaster>,
     material: Box<dyn Material>,
-    lightning: Box<dyn Lightning>,
-    // render_texture: RefCell<Texture>,
-    // render_texture_size: (usize, usize)
+    lightning: Box<dyn Lightning>
 }
 
 impl Renderer {
-    pub fn new(
-        mut camera: Camera,
-        raycaster: Box<dyn Raycaster>,
-        material: Box<dyn Material>,
-        lightning: Box<dyn Lightning>,
-        pixel_canvas: PixelCanvas
-        // render_texture: Texture
-    ) -> Renderer {
-        // let render_texture_size = query_texture_size(&render_texture);
-
-        camera.set_aspect_ratio(pixel_canvas.get_aspect_ratio());
-
-        Renderer {
-            pixel_canvas: RefCell::new(pixel_canvas),
-            camera,
-            raycaster,
-            material,
-            lightning,
-            // render_texture: RefCell::new(render_texture),
-            // render_texture_size
-        }
-    }
-
     pub fn get_pixel_canvas(&self) -> Ref<PixelCanvas> {
         self.pixel_canvas.borrow()
     }
 
-    pub fn set_pixel_canvas(&mut self, pixel_canvas: PixelCanvas) {
-        self.pixel_canvas = RefCell::new(pixel_canvas);
-        self.camera.set_aspect_ratio(self.pixel_canvas.borrow().get_aspect_ratio());
-    }
-
     pub fn get_mut_camera(&mut self) -> &mut Camera {
         &mut self.camera
-    }
-
-    pub fn set_camera(&mut self, camera: Camera) {
-        self.camera = camera;
-        self.camera.set_aspect_ratio(self.pixel_canvas.borrow().get_aspect_ratio());
     }
 
     pub fn get_mut_raycaster(&mut self) -> &mut dyn Raycaster {
@@ -85,43 +51,11 @@ impl Renderer {
         self.lightning = lightning;
     }
 
-    // pub fn get_mut_render_texture(&mut self) -> &mut Texture {
-    //     self.render_texture.get_mut()
-    // }
-
-    // pub fn set_render_texture(&mut self, render_texture: Texture) {
-    //     let query = render_texture.query();
-    //     let render_texture_size = (query.width as usize, query.height as usize);
-
-    //     self.render_texture = RefCell::new(render_texture);
-    //     self.render_texture_size = render_texture_size;
-    //     self.camera.set_aspect_ratio(calc_aspect_ratio(self.render_texture_size));
-    // }
-
     pub fn render(&mut self) {
         let width = self.pixel_canvas.borrow().get_width();
         let height = self.pixel_canvas.borrow().get_height();
         let w = width as f32;
         let h = height as f32;
-
-        // self.render_texture.borrow_mut().with_lock(None, |data, pitch| {
-        //     let pixel_pitch = pitch / width;
-
-        //     for y in 0..height {
-        //         for x in 0..width {
-        //             let pixel = self.render_pixel(
-        //                 (x as f32) / w * 2.0 - 1.0,
-        //                 (h - (y as f32)) / h * 2.0 - 1.0
-        //             );
-
-        //             let idx = x * pixel_pitch + pitch * y;
-        //             data[idx] = pixel.a;
-        //             data[idx + 1] = pixel.b;
-        //             data[idx + 2] = pixel.g;
-        //             data[idx + 3] = pixel.r;
-        //         }
-        //     }
-        // }).expect("Unable to render in texture");
 
         self.pixel_canvas.borrow_mut().update_with(|mut writer| {
             for y in 0..height {
@@ -156,11 +90,47 @@ impl Renderer {
     }
 }
 
-// fn query_texture_size(texture: &Texture) -> (usize, usize) {
-//     let q = texture.query();
-//     (q.width as usize, q.height as usize)
-// }
+pub fn build_renderer(
+    gl: Rc<glow::Context>, texture_map: &mut dyn ImguiTextureMap, render_size: [usize; 2]
+) -> Result<Renderer, String> {
+    let mut camera = Camera::default();
+    camera.get_mut_transform().set_position(&Vector3::new(0.0, 0.0, -2.0));
+    camera.set_aspect_ratio((render_size[0] as f32) / (render_size[1] as f32));
+    
+    let mut raycaster = Box::new(
+        BoxRaycaster::new(&Vector3::new(1.0, 1.0, 1.0))
+    );
+    raycaster.get_mut_tranform().set_rotation(
+        &Vector3::new(45.0_f32.to_radians(), 45.0_f32.to_radians(), 0.0_f32.to_radians())
+    );
 
-// fn calc_aspect_ratio(texture_size: (usize, usize)) -> f32 {
-//     (texture_size.0 as f32) / (texture_size.1 as f32)
-// }
+    let material = Box::new(CheckerMaterial { 
+        scale: 1.0,
+        first_color: Color::new(255, 255, 255),
+        second_color: Color::new(127, 127, 127)
+    });
+
+    let lightning = Box::new(
+        DiffuseDirectLightning::new(
+            &Vector3::new(1.0, -1.0, 1.0),
+            &Color::new(255, 255, 255),
+            &Color::new(40, 40, 40)
+        )
+    );
+
+    let pixel_canvas = RefCell::new(
+        PixelCanvas::new(
+            gl, texture_map, render_size[0], render_size[1]
+        )?
+    );
+
+    let renderer = Renderer {
+        pixel_canvas,
+        camera,
+        raycaster,
+        material,
+        lightning
+    };
+
+    Ok(renderer)
+}
