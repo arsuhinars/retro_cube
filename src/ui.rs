9b3abf6812879aeda43ffe1663cf2de74473da33
arsuhinars <arsuhinars@gmail.com>
2023-09-25 22:37:39 +0400

    Added somu UI util functions

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,8 @@
 use imgui_glow_renderer::AutoRenderer as ImguiRenderer;
 use imgui_sdl2_support::SdlPlatform as ImguiPlatform;
 
+use crate::utils::vector::Vector3;
+
 pub fn build_imgui(
     gl: glow::Context
 ) -> Result<(imgui::Context, ImguiPlatform, ImguiRenderer), String> {
@@ -20,4 +22,20 @@ pub fn build_imgui(
 
 pub trait ImguiEditor {
     fn draw_ui(&mut self, ui: &imgui::Ui);
+}
+
+const DEFAULT_DRAG_SPEED: f32 = 0.02;
+
+pub fn drag_float(ui: &imgui::Ui, label: &str, value: &mut f32) -> bool {
+    imgui::Drag::new(label).speed(DEFAULT_DRAG_SPEED).build(ui, value)
+}
+
+pub fn drag_float3(ui: &imgui::Ui, label: &str, value: &mut Vector3) -> bool {
+    let mut arr = Into::<[f32; 3]>::into(*value);
+    let result = imgui::Drag::new(label)
+        .speed(DEFAULT_DRAG_SPEED)
+        .build_array(ui, &mut arr);
+    *value = Into::into(arr);
+
+    return result;
 }
